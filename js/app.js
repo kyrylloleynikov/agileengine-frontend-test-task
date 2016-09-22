34a46c9a9a99796ad2f5405810c7c671eff2908d
Cyril Oleynikov <korr0101@gmail.com>
2016-09-22 03:05:57 +0300

    added backbone, initialise for all js frameworks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
+// Angular
+const angular = require('angular');
+
+// React
+const React = require('react'),
+    ReactDOM = require('react-dom');
+
+// jQuery
+const $ = require('jquery');
+
+// Backbone
+const Backbone = require('backbone');
+
+// Knockout
+const ko = require('knockout');
+
 // JS code starts here
